4e4f24ed4402ec6dee8ba924a3ff25023bf360c9
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-12-22 19:09:48 +0200

    Added a basic colorpicker visuals

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,23 @@
 <template>
-    <div id="app" :style="color" >
-        <h1>Colorpicker</h1>
+    <div id="app" :style="color">
+        <div class='container'>
+            <h1>Colorpicker</h1>
+            <ColorPresets />
+        </div>
     </div>
 </template>
 
 <script>
+import ColorPresets from './components/ColorPresets'
+
     export default {
         name: 'App',
-        components: {},
+        components: {
+            ColorPresets,
+        },
         data() {
             return {
-                currentColor: '#e309ca',
+                currentColor: '#2b2b2b',
             }
         },
         computed: {
@@ -25,7 +32,7 @@
 
 <style>
     #app {
-        font-family: Lato;
+        font-family:'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
         color: white;
         background-color: var(--color);
         width: 100vw;
@@ -36,5 +43,15 @@
         display: flex;
         align-items: center;
         justify-content: center;
+        text-align: center;
+    }
+    h1 {
+        text-overflow: ellipsis;
+        margin: auto;
+        padding-bottom: 1em;
+    }
+    .container {
+        width: 55%;
+        margin: auto;
     }
 </style>
---
+++ src/components/ColorPresets.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container">
+        <div class="wrapper">
+            <div class='colorButton' :style="{backgroundColor: 'blue'}"></div>
+            <div class='colorButton' :style="{backgroundColor: 'red'}"></div>
+            <div class='colorButton' :style="{backgroundColor: 'green'}"></div>
+            <div class='colorButton' :style="{backgroundColor: 'yellow'}"></div>
+            <div class='colorButton' :style="{backgroundColor: 'purple'}"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ColorPresets',
+    }
+</script>
+
+<style scoped>
+    .container {
+        border-radius: 10px;
+        background-color: rgb(0, 0, 0, 0.6);
+        width: auto;
+        height: auto;
+    }
+    .wrapper {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        height: 75px;
+    }
+    .colorButton {
+        width: 40%;
+        height: 70%;
+        margin: auto;
+        border-radius: 50%;
+    }
+    .colorButton:hover {
+        border: 3px solid white;
+        box-shadow: 0 0 10px #9ecaed;
+        transition: 0.5s;
+    }
+</style>
