05d58032a384104df0866bd8510f1d1eecea7352
Lassi Timoskainen <timoskainen.lassi@gmail.com>
2020-12-22 16:55:08 +0200

    Inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,40 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app" :style="color" >
+        <h1>Colorpicker</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {},
+        data() {
+            return {
+                currentColor: '#e309ca',
+            }
+        },
+        computed: {
+            color() {
+                return {
+                    '--color': this.currentColor,
+                }
+            },
+        },
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Lato;
+        color: white;
+        background-color: var(--color);
+        width: 100vw;
+        height: 100vh;
+        position: absolute;
+        left: 0;
+        top: 0;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
 </style>
